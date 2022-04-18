832c420a584dbb44eb293f6360f319e522f1c4c0
gavin443 <102693773+gavin443@users.noreply.github.com>
2022-04-18 12:32:38 +0100

    Add files via upload

---
+++ tvout/script.ts
@@ -0,0 +1,40 @@
+class TransferOutWidget {
+  private maxPensionablePayInput: HTMLInputElement;
+  private nextSection: NodeListOf<Element>;
+  private pageCount = 0;
+
+  constructor(private widgetId: string) {
+    this.init();
+  }
+
+  private init(): void {
+    this.maxPensionablePayInput = document.getElementById(
+      `max-pensionable-pay-input-${this.widgetId}`
+    ) as HTMLInputElement;
+    this.nextSection = document.querySelectorAll(
+      ".next-section"
+    ) as NodeListOf<Element>;
+    this.setupListeners();
+  }
+
+  private setupListeners(): void {
+    //this.maxPensionablePayInput.addEventListener("input", () => {});
+    this.nextSection.forEach((node) => {
+      node.addEventListener("click", () => {
+        (<HTMLElement>(
+          document.querySelector(`[data-tranferpage='${this.pageCount}']`)
+        )).classList.add("d-none");
+        (<HTMLElement>(
+          document.querySelector(`[data-tranferpage='${this.pageCount + 1}']`)
+        )).classList.remove("d-none");
+        (<HTMLElement>(
+          document.querySelector(`[data-transferstep='${this.pageCount}']`)
+        )).classList.replace("active", "completed");
+        (<HTMLElement>(
+          document.querySelector(`[data-transferstep='${this.pageCount + 1}']`)
+        )).classList.add("active");
+        this.pageCount++;
+      });
+    });
+  }
+}
