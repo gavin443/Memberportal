d61db31b431780e32b586c5be73284dfa6952d76
gavin443 <102693773+gavin443@users.noreply.github.com>
2022-04-22 17:32:56 +0100

    Add files via upload

--- tvout/script.ts
+++ tvout/script.ts
@@ -1,39 +1,118 @@
 class TransferOutWidget {
-  private maxPensionablePayInput: HTMLInputElement;
-  private nextSection: NodeListOf<Element>;
-  private pageCount = 0;
-
+  private transferOptions: NodeListOf<HTMLInputElement>;
+  private turnSection: NodeListOf<Element>;
+  private pageCount: number;
   constructor(private widgetId: string) {
     this.init();
+    this.pageCount = 0;
   }
 
   private init(): void {
-    this.maxPensionablePayInput = document.getElementById(
-      `max-pensionable-pay-input-${this.widgetId}`
-    ) as HTMLInputElement;
-    this.nextSection = document.querySelectorAll(
-      ".next-section"
+    this.transferOptions = document.querySelectorAll(
+      'input[name="transfer-option"], input[name^="request-"]'
+    ) as NodeListOf<HTMLInputElement>;
+
+    this.turnSection = document.querySelectorAll(
+      ".next-section,.prev-section"
     ) as NodeListOf<Element>;
     this.setupListeners();
   }
 
+  private validateOptions(): void {
+    //some data-set values can be the reverse of the data-set, for consistency use a data-tag
+    const canShow: boolean =
+      (<HTMLInputElement>(
+        document.querySelector(
+          `[data-tranferpage='${this.pageCount}'] input[name^="request-"]:checked`
+        )
+      ))?.dataset.text == "true";
+
+    const noneChecked =
+      document.querySelectorAll(
+        `[data-tranferpage='${this.pageCount}'] input[name^="request-"]:checked`
+      ).length > 0;
+
+    console.log(noneChecked);
+
+    (<HTMLInputElement>(
+      document.querySelector(
+        `[data-tranferpage='${this.pageCount}'] .step-text`
+      )
+    ))?.classList.toggle("d-none", canShow);
+
+    console.log(this.pageCount);
+    switch (this.pageCount) {
+      case 0:
+        (<HTMLInputElement>(
+          document.querySelector(
+            `[data-tranferbtn='${this.pageCount}'].request`
+          )
+        ))?.classList.toggle("d-none", canShow);
+        (<HTMLInputElement>(
+          document.querySelector(
+            `[data-tranferbtn='${this.pageCount}'].next-section`
+          )
+        ))?.classList.toggle("d-none", !canShow);
+        break;
+      case 3:
+        (<HTMLInputElement>(
+          document.querySelector(
+            `[data-tranferpage='${this.pageCount}'] .read-more`
+          )
+        ))?.classList.toggle("d-none", !canShow);
+        (<HTMLInputElement>(
+          document.querySelector(
+            `[data-tranferbtn='${this.pageCount}'].next-section`
+          )
+        ))?.classList.toggle("d-none", canShow);
+        break;
+      default:
+        console.log(
+          document.querySelector(
+            `[data-tranferbtn='${this.pageCount}'].next-section`
+          )
+        );
+        (<HTMLInputElement>(
+          document.querySelector(
+            `[data-tranferbtn='${this.pageCount}'].next-section`
+          )
+        ))?.classList.toggle("d-none", !noneChecked);
+        break;
+    }
+  }
+
   private setupListeners(): void {
-    //this.maxPensionablePayInput.addEventListener("input", () => {});
-    this.nextSection.forEach((node) => {
+    this.transferOptions.forEach((node) => {
       node.addEventListener("click", () => {
+        this.validateOptions();
+      });
+    });
+
+    this.turnSection.forEach((node) => {
+      node.addEventListener("click", (e) => {
         (<HTMLElement>(
           document.querySelector(`[data-tranferpage='${this.pageCount}']`)
         )).classList.add("d-none");
-        (<HTMLElement>(
-          document.querySelector(`[data-tranferpage='${this.pageCount + 1}']`)
-        )).classList.remove("d-none");
+        if ((<Element>e.target).matches(".prev-section")) {
+          (<HTMLElement>(
+            document.querySelector(`[data-transferstep='${this.pageCount}']`)
+          )).classList.remove("active");
+          this.pageCount--;
+          (<HTMLElement>(
+            document.querySelector(`[data-transferstep='${this.pageCount}']`)
+          )).classList.replace("completed", "active");
+        } else {
+          (<HTMLElement>(
+            document.querySelector(`[data-transferstep='${this.pageCount}']`)
+          )).classList.replace("active", "completed");
+          this.pageCount++;
+        }
         (<HTMLElement>(
           document.querySelector(`[data-transferstep='${this.pageCount}']`)
-        )).classList.replace("active", "completed");
+        )).classList?.add("active");
         (<HTMLElement>(
-          document.querySelector(`[data-transferstep='${this.pageCount + 1}']`)
-        )).classList.add("active");
-        this.pageCount++;
+          document.querySelector(`[data-tranferpage='${this.pageCount}']`)
+        ))?.classList.remove("d-none");
       });
     });
   }
