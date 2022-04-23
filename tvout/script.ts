6d9633c32b7254a72ee31057834bdd4a880ea02b
gavin443 <102693773+gavin443@users.noreply.github.com>
2022-04-23 18:27:08 +0100

    Add files via upload

--- tvout/script.ts
+++ tvout/script.ts
@@ -23,60 +23,53 @@ class TransferOutWidget {
     const canShow: boolean =
       (<HTMLInputElement>(
         document.querySelector(
-          `[data-tranferpage='${this.pageCount}'] input[name^="request-"]:checked`
+          `[data-transferpage='${this.pageCount}'] input[name^="request-"]:checked`
         )
       ))?.dataset.text == "true";
 
     const noneChecked =
       document.querySelectorAll(
-        `[data-tranferpage='${this.pageCount}'] input[name^="request-"]:checked`
+        `[data-transferpage='${this.pageCount}'] input[name^="request-"]:checked`
       ).length > 0;
-
-    console.log(noneChecked);
 
     (<HTMLInputElement>(
       document.querySelector(
-        `[data-tranferpage='${this.pageCount}'] .step-text`
+        `[data-transferpage='${this.pageCount}'] .step-text`
       )
     ))?.classList.toggle("d-none", canShow);
 
-    console.log(this.pageCount);
     switch (this.pageCount) {
       case 0:
-        (<HTMLInputElement>(
-          document.querySelector(
-            `[data-tranferbtn='${this.pageCount}'].request`
+        document
+          .querySelector<HTMLInputElement>(
+            `[data-transferbtn='${this.pageCount}'].request`
           )
-        ))?.classList.toggle("d-none", canShow);
-        (<HTMLInputElement>(
-          document.querySelector(
-            `[data-tranferbtn='${this.pageCount}'].next-section`
+          ?.classList.toggle("d-none", canShow);
+
+        document
+          .querySelector<HTMLInputElement>(
+            `[data-transferbtn='${this.pageCount}'].next-section`
           )
-        ))?.classList.toggle("d-none", !canShow);
+          ?.classList.toggle("d-none", !canShow);
         break;
       case 3:
-        (<HTMLInputElement>(
-          document.querySelector(
-            `[data-tranferpage='${this.pageCount}'] .read-more`
+        document
+          .querySelector<HTMLInputElement>(
+            `[data-transferpage='${this.pageCount}'] .read-more`
           )
-        ))?.classList.toggle("d-none", !canShow);
-        (<HTMLInputElement>(
-          document.querySelector(
-            `[data-tranferbtn='${this.pageCount}'].next-section`
+          ?.classList.toggle("d-none", !canShow);
+        document
+          .querySelector<HTMLInputElement>(
+            `[data-transferbtn='${this.pageCount}'].next-section`
           )
-        ))?.classList.toggle("d-none", canShow);
+          ?.classList.toggle("d-none", canShow);
         break;
       default:
-        console.log(
-          document.querySelector(
-            `[data-tranferbtn='${this.pageCount}'].next-section`
+        document
+          .querySelector<HTMLInputElement>(
+            `[data-transferbtn='${this.pageCount}'].next-section`
           )
-        );
-        (<HTMLInputElement>(
-          document.querySelector(
-            `[data-tranferbtn='${this.pageCount}'].next-section`
-          )
-        ))?.classList.toggle("d-none", !noneChecked);
+          ?.classList.toggle("d-none", !noneChecked);
         break;
     }
   }
@@ -90,29 +83,35 @@ class TransferOutWidget {
 
     this.turnSection.forEach((node) => {
       node.addEventListener("click", (e) => {
-        (<HTMLElement>(
-          document.querySelector(`[data-tranferpage='${this.pageCount}']`)
-        )).classList.add("d-none");
+        document
+          .querySelector<HTMLElement>(`[data-transferpage='${this.pageCount}']`)
+          .classList.add("d-none");
         if ((<Element>e.target).matches(".prev-section")) {
-          (<HTMLElement>(
-            document.querySelector(`[data-transferstep='${this.pageCount}']`)
-          )).classList.remove("active");
+          document
+            .querySelector<HTMLElement>(
+              `[data-transferstep='${this.pageCount}']`
+            )
+            .classList.remove("active");
           this.pageCount--;
-          (<HTMLElement>(
-            document.querySelector(`[data-transferstep='${this.pageCount}']`)
-          )).classList.replace("completed", "active");
+          document
+            .querySelector<HTMLElement>(
+              `[data-transferstep='${this.pageCount}']`
+            )
+            .classList.replace("completed", "active");
         } else {
-          (<HTMLElement>(
-            document.querySelector(`[data-transferstep='${this.pageCount}']`)
-          )).classList.replace("active", "completed");
+          document
+            .querySelector<HTMLElement>(
+              `[data-transferstep='${this.pageCount}']`
+            )
+            .classList.replace("active", "completed");
           this.pageCount++;
         }
-        (<HTMLElement>(
-          document.querySelector(`[data-transferstep='${this.pageCount}']`)
-        )).classList?.add("active");
-        (<HTMLElement>(
-          document.querySelector(`[data-tranferpage='${this.pageCount}']`)
-        ))?.classList.remove("d-none");
+        document
+          .querySelector<HTMLElement>(`[data-transferstep='${this.pageCount}']`)
+          .classList?.add("active");
+        document
+          .querySelector<HTMLElement>(`[data-transferpage='${this.pageCount}']`)
+          ?.classList.remove("d-none");
       });
     });
   }
